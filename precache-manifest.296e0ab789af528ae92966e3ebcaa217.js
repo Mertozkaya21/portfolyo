fa849e1fad12e834233c007a08c6381549e082da
Mert Özkaya <mertozkaya2k1@gmail.com>
2025-06-01 16:19:33 +0300

    Updates

---
+++ precache-manifest.296e0ab789af528ae92966e3ebcaa217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e473939e91d6ba854f1bc68b3f7b628",
+    "url": "/portfolyo/index.html"
+  },
+  {
+    "revision": "f34b80cb657a5f87ea73",
+    "url": "/portfolyo/static/css/main.77ce6007.chunk.css"
+  },
+  {
+    "revision": "b67d47b430f9d8e69383",
+    "url": "/portfolyo/static/js/2.2c212bfb.chunk.js"
+  },
+  {
+    "revision": "8a721b77919ac5b264ba0684d0da5769",
+    "url": "/portfolyo/static/js/2.2c212bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63352698f170f1c183a4",
+    "url": "/portfolyo/static/js/3.6cf44e4c.chunk.js"
+  },
+  {
+    "revision": "f34b80cb657a5f87ea73",
+    "url": "/portfolyo/static/js/main.50570b74.chunk.js"
+  },
+  {
+    "revision": "f9d3185df3f229c28f24",
+    "url": "/portfolyo/static/js/runtime-main.fddc7212.js"
+  }
+]);
